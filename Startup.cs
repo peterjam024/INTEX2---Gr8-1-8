942823913e89b942f100b8bd360ce15e357b908f
Scott Olson <scottolsonofficial@gmail.com>
2022-04-07 14:27:50 -0600

    Environment Variables Added

--- Startup.cs
+++ Startup.cs
@@ -48,9 +48,10 @@ namespace CrashySmashy
             });
             services.AddDbContext<CrashesContext>(options =>
             {
-                //options.UseSqlite(Configuration["ConnectionStrings:MyConnection"]);
+                
+                //options.UseMySql(Configuration["CrashesDBConnectionString"]);
                 options.UseMySql(Configuration["ConnectionStrings:MyConnection"]);
-
+                
             });
             services.AddSingleton<InferenceSession>(
             new InferenceSession("wwwroot/machineLearning/reg_crash_model.onnx")
@@ -58,8 +59,9 @@ namespace CrashySmashy
 
 
             //add the dbContext of Identity!
-            services.AddDbContext<AppIdentityDBContext>(options => options.UseMySql(Secrets.GetSecret()));
+           // services.AddDbContext<AppIdentityDBContext>(options => options.UseMySql(Configuration["crashesUsersConnectionString"]));
 
+            services.AddDbContext<AppIdentityDBContext>(options => options.UseMySql(Configuration["ConnectionStrings:IdentityDBConnection"]));
 
             //this goes along our datbase above = serves as helping with login stuff
             services.AddIdentity<IdentityUser, IdentityRole>()
