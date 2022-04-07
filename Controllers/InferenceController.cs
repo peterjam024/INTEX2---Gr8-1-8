05003079f7f14e3475a0ee065864b358116639bd
James Peterson <jamespetersonwrestling@gmail.com>
2022-04-07 16:22:02 -0600

    this better save itself

--- Controllers/InferenceController.cs
+++ Controllers/InferenceController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using CrashySmashy.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
@@ -24,8 +25,37 @@ namespace aspnetcore.Controllers
 
         //
         [HttpPost]
-        public ActionResult Score(crash_severity_data data)
+        public ActionResult Score(crash_severity_data data, IFormCollection form)
         {
+
+            //          MAKE OTHER .CS MODELS TO GIVE YOU OTHER CLASSES TO USE
+
+            decimal city = Convert.ToDecimal(form["City"]);
+            decimal county = Convert.ToDecimal(form["County"]);
+
+
+
+            if (city == 1)
+                data.city_SALT_LAKE_CITY = 1;
+            else if (city == 2)
+                data.city_WEST_VALLEY_CITY = 1;
+            else
+                data.city_Other = 1;
+
+
+
+            if (county == 1)
+                data.county_name_SALT_LAKE = 1;
+            else if (county == 2)
+                data.county_name_WEBER = 1;
+            else if (county == 3)
+                data.county_name_UTAH = 1;
+            else
+                data.county_name_Other = 1;
+
+
+
+
             var result = _session.Run(new List<NamedOnnxValue>
             {
                 NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
