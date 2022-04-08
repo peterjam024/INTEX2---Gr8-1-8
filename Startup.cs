8f001791cb6305acf966f703287808d4c9f9d857
James Peterson <jamespetersonwrestling@gmail.com>
2022-04-08 00:27:32 -0600

    blap

--- Startup.cs
+++ Startup.cs
@@ -13,7 +13,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using CrashySmashy.Infrastructure;
 
 namespace CrashySmashy
 {
